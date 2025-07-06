e4f2926207885b0fc1d006b0806193dd683e016f
coder-student24 <march_0120@qq.com>
2025-07-06 14:00:59 +0800

    Add files via upload

---
+++ 作业6/main.py
@@ -0,0 +1,15 @@
+#with open('new.txt','w+') as i:
+# print(a)'''
+'''i.write('hello word/n')
+print(i.read())'''
+
+#with open('new.txt','a') as f:
+  # f.write('whit is loves')
+   #f.close()
+
+#with open('new.txt','r+') as f:
+ #  f.write('?o?')
+ #  print(f.read())
+
+
+
